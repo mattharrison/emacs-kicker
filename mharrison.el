bfba0055e0eac17ff737688572ca87ba81852556
matt <matthewharrison@gmail.com>
2011-08-21 21:19:22 -0600

    add camel case toggle

--- mharrison.el
+++ mharrison.el
@@ -468,6 +468,34 @@ and choosing a simple theme."
 ;; (require 'python)
 
 
+;; https://www.bunkus.org/blog/2009/12/switching-identifier-naming-style-between-camel-case-and-c-style-in-emacs/
+(defun mo-toggle-identifier-naming-style ()
+  "Toggles the symbol at point between C-style naming,
+    e.g. `hello_world_string', and camel case,
+    e.g. `HelloWorldString'."
+  (interactive)
+  (let* ((symbol-pos (bounds-of-thing-at-point 'symbol))
+	 case-fold-search symbol-at-point cstyle regexp func)
+    (unless symbol-pos
+      (error "No symbol at point"))
+    (save-excursion
+      (narrow-to-region (car symbol-pos) (cdr symbol-pos))
+      (setq cstyle (string-match-p "_" (buffer-string))
+	    regexp (if cstyle "\\(?:\\_<\\|_\\)\\(\\w\\)" "\\([A-Z]\\)")
+	    func (if cstyle
+		     'capitalize
+		   (lambda (s)
+		     (concat (if (= (match-beginning 1)
+				    (car symbol-pos))
+				 ""
+			       "_")
+			     (downcase s)))))
+      (goto-char (point-min))
+      (while (re-search-forward regexp nil t)
+	(replace-match (funcall func (match-string 1))
+		       t nil))
+      (widen))))
+
 ;; from https://github.com/nflath/emacs-repos/blob/master/internal/python.el
 (defun python-insert-end-dunder ()
   "Appends __ to the current word if it started with __."
