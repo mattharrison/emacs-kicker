a9e8fabfcc30abfdd56e78ce2b570b6d8252cdbe
matt harrison <matthewharrison@gmail.com>
2012-12-17 14:52:05 -0700

    Update mharrison.el
    Move backups to ~/.emacs-backups

--- mharrison.el
+++ mharrison.el
@@ -220,6 +220,14 @@
 
 (el-get 'sync my-packages)
 
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory ".emacs-backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
 ;; Use the clipboard, pretty please, so that copy/paste "works"
 (setq x-select-enable-clipboard t)
 
